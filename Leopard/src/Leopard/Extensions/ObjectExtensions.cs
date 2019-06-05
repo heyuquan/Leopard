3e2d7e11f603cb6f65f7a6644169fa4f3789bf82
beek_hyq <beek_hyq@meten.com>
2019-06-05 19:28:47 +0800

    url，dispose

--- Leopard/src/Leopard/Extensions/ObjectExtensions.cs
+++ Leopard/src/Leopard/Extensions/ObjectExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -308,6 +309,5 @@ namespace Leopard.Extensions
                 return ms.ToArray();
             }
         }
-
     }
 }
