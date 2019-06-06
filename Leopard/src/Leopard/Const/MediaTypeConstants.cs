69faff52e01385404396e21a8cb4afd3f9431d47
beek_hyq <beek_hyq@meten.com>
2019-06-06 19:38:14 +0800

    const

---
+++ Leopard/src/Leopard/Const/MediaTypeConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leopard
+{
+    public class MediaTypeConstants
+    {
+        public const string Xml = "application/xml";
+
+        public const string Json = "application/json";
+
+        public const string Bson = "application/bson";
+
+        public const string MessagePack = "application/x-msgpack";
+
+        public const string ProtoBuf = "application/x-protobuf";
+
+        public const string FormUrl = "application/x-www-form-urlencoded";
+    }
+}
